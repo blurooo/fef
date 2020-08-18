9e088c3cf0f9b2a5569deb2e1cb90b77b9be9dda
blurooochen <blurooochen@tencent.com>
2020-08-18 14:44:09 +0800

    fix: code is undefined

--- dist/index.js
+++ dist/index.js
@@ -1034,6 +1034,25 @@ module.exports = require("child_process");
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -1043,14 +1062,14 @@ var path_1 = __importDefault(__webpack_require__(622));
 var plugin_1 = __webpack_require__(125);
 var yaml_1 = __webpack_require__(898);
 var pkg_1 = __webpack_require__(458);
-var core_1 = __importDefault(__webpack_require__(51));
+var core = __importStar(__webpack_require__(51));
 try {
     var home = os_1.default.homedir();
     var workdir = path_1.default.join(home, '.fef');
     var dependency = path_1.default.join(workdir, 'universal-package.json');
     var pkgRelation = new pkg_1.UniversalPkg(dependency);
-    var run = core_1.default.getInput("run");
-    var params = core_1.default.getInput("params");
+    var run = core.getInput("run");
+    var params = core.getInput("params");
     var runSplit = run.split('@');
     var pkg = runSplit[0];
     if (!pkg) {
